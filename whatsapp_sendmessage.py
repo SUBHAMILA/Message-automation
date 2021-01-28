87ff365e066b24826fa7c5d3a1a4154d2824fa2a
Subhashini H <38394546+SUBHAMILA@users.noreply.github.com>
2021-01-28 14:45:02 +0530

    Update whatsapp_sendmessage.py

--- whatsapp_sendmessage.py
+++ whatsapp_sendmessage.py
@@ -6,19 +6,17 @@ from selenium.webdriver.common.by import By
 import time
 import sys
 
-# Replace below path with the absolute path
-# to chromedriver in your computer
+# Replace below path with the absolute path to chromedriver in your computer
 driver = webdriver.Chrome('./chromedriver')
 
 driver.get("https://web.whatsapp.com/")
 wait = WebDriverWait(driver, 600)
 
-# Replace 'Friend's Name' with the name of your friend 
-# or the name of a group 
-target = '"Tharkie"'
+# Replace 'Friend's Name' with the name of your friend or the name of a group 
+target = '"Aussie"'
 
 # Replace the below string with your own message
-string = "I am sad!"
+string = "Hi, How are you?"
 
 x_arg = '//span[contains(@title,' + target + ')]'
 group_title = wait.until(EC.presence_of_element_located((
@@ -28,7 +26,7 @@ group_title.click()
 
 message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
 
-for i in range(1000):
+for i in range(10):
 	message.send_keys(string + Keys.ENTER)
 
 sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
